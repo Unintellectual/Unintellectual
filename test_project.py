73491f3512683cabbc05bc10df28be7621ce5a7f
Unintellectual <Unintellectual@users.noreply.github.com>
2024-03-19 02:45:56 +0800

    automated commit by submit50

--- test_project.py
+++ test_project.py
@@ -1,23 +1,18 @@
+import pytest
 from unittest.mock import patch
-from project import show_score, player, enemy
+import project  # Assuming your main code is in a file named 'main.py'
 
-@patch('pygame.font.Font.render')
-@patch('pygame.Surface.blit')
-def test_show_score(mock_blit, mock_render):
-    x, y = 10, 10
-    score_value = 10
-    show_score(x, y)
-    mock_render.assert_called_once_with("Score : 10", True, (255, 255, 255))
-    mock_blit.assert_called_once()
+def test_some_func():
+    with patch("builtins.print") as mocked_print:
+        project.some_func()
+        mocked_print.assert_called_once()
 
-@patch('pygame.Surface.blit')
-def test_player(mock_blit):
-    x, y = 100, 100
-    player(x, y)
-    mock_blit.assert_called_once()
+def test_somes_func():
+    with patch("builtins.print") as mocked_print:
+        project.somes_func()
+        mocked_print.assert_called_once()
 
-@patch('pygame.Surface.blit')
-def test_enemy(mock_blit):
-    x, y, b = 200, 200, 0
-    enemy(x, y, b)
-    mock_blit.assert_called_once()
+def test_somed_func():
+    with patch("builtins.print") as mocked_print:
+        project.somed_func()
+        mocked_print.assert_called_once()
